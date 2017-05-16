b94d5bcea09b5bd0217632af77c82abf64ab96b7
Alex Maybaum <amaybaum@gmail.com>
2017-05-16 13:39:38 +0300

    bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='WssPlugin_integration',
-    version='1.1.0',
+    version='1.1.1',
     description='Wss python pluging for creating/updating wss organization inventory',
     author='Yossi Weinberg',
     author_email='yossi.weinberg@whitesourcesoftware.com',
