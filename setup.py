e954d43a1dd9a804fe816e1b1716a55d5082f572
raznitzan <raz.nitzan@whitesourcesoftware.com>
2018-05-06 17:51:05 +0300

    WSE-441 - Change version of dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Wss python pluging for creating/updating wss organization inventory',
     author='Yossi Weinberg',
     author_email='yossi.weinberg@whitesourcesoftware.com',
-    install_requires=["requests == 2.13.0", "enum34 == 1.1.6", "jsonpickle == 0.9.4", "mock == 2.0.0", "pip"],
+    install_requires=["requests == 2.14.2", "enum34 == 1.1.6", "jsonpickle == 0.9.4", "mock == 2.0.0", "pip"],
     packages=['plugin', 'agent', 'agent.client', 'agent.api', 'agent.api.dispatch', 'agent.api.model'],
     include_package_data=True,
     license="Apache 2.0"
