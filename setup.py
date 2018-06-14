612cd4c39b6846edf03906715dbcc81360b3a96e
raznitzan <raz.nitzan@whitesourcesoftware.com>
2018-06-14 14:57:58 +0300

    WSE-522 - Change version to requests dependency

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='wss_plugin',
-    version='18.4.2',
+    version='18.6.2',
     description='Wss python pluging for creating/updating wss organization inventory',
     author='Yossi Weinberg',
     author_email='yossi.weinberg@whitesourcesoftware.com',
-    install_requires=["requests == 2.14.2", "enum34 == 1.1.6", "jsonpickle == 0.9.4", "mock == 2.0.0", "pip"],
+    install_requires=["requests >= 2.14.2", "enum34 == 1.1.6", "jsonpickle == 0.9.4", "mock == 2.0.0", "pip"],
     packages=['plugin', 'agent', 'agent.client', 'agent.api', 'agent.api.dispatch', 'agent.api.model'],
     include_package_data=True,
     license="Apache 2.0"
