f4b2bff1969ca432b0b6a9a157ccce6a989d221d
GeorgeAzzam <george.azzam@whitesourcesoftware.com>
2018-07-04 11:20:04 +0300

    Update version to 18.6.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wss_plugin',
-    version='18.6.2',
+    version='18.6.3',
     description='Wss python pluging for creating/updating wss organization inventory',
     author='Yossi Weinberg',
     author_email='yossi.weinberg@whitesourcesoftware.com',
