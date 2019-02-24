5f6baa197e671796d6df1644b4b402129e8421cb
Raz Nitzan <raz.nitzan@whitesourcesofware.com>
2019-02-24 09:42:18 +0200

    Change version to 19.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wss_plugin',
-    version='18.10.3',
+    version='19.2.1',
     description='Wss python pluging for creating/updating wss organization inventory',
     author='Yossi Weinberg',
     author_email='yossi.weinberg@whitesourcesoftware.com',
